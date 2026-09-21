3f317e2e9a45acdaedcf042a427e7659cc5e64be
dhiyaneshwarank-star <dhiyaneshwarank@gmail.com>
2026-09-21 12:48:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+app.py 
+
+from flask import Flask 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/') 
+def home(): 
+    return "<h1>Hello from Cloud!</h1><p>Deployed on Render</p>" 
+ 
+@app.route('/status') 
+def status(): 
+    return { 
+        "status": "running", 
+        "server": "Render" 
+    } 
+ 
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000) 
